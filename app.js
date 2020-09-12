d8951785c7d21174903d23c476dd798620d48c74
Corey DeLoach <coreydeloach@Coreys-MBP.attlocal.net>
2020-09-12 09:42:46 -0500

    advancdeJSdrills

---
+++ app.js
@@ -0,0 +1,43 @@
+// name = 'Corey';
+// var name;
+// console.log(name);
+
+// setName();
+// function setName (){
+//     var name = 'Corey';
+//     console.log(name);
+// }
+
+// console.log('YO');
+// let find = average(4,40);
+// console.log('I see now', find);
+// function average (x,y){
+//     console.log('I am a function');
+//     var answer = (x + y) / 2
+//     return answer;
+// }
+
+// let fruits = ['oranges', 'apples', 'bananas'];
+// // let favFruit;
+// function fruitDisplay() {
+//     console.log(fruits[0]);
+//     favFruit = fruits[2];
+//     printFav();
+//     console.log(leastFav);
+//     function printFav() {
+//         console.log(favFruit);
+//         let leastFav = fruits[1];
+//         // let favFruit = fruits[2];
+//     }
+// }
+// fruitDisplay();
+
+// doStuff();
+// function doStuff (){
+//     console.log('Hello' + ' Corey');
+// }
+
+// let doTask = function doTask(){
+//     alert('WTF');
+// }
+// doTask();
